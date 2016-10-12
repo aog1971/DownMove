da204b729d7bd0b406861bf2c34d2615b89a35eb
antaog <antaog@hotmail.com>
2016-10-12 18:33:14 +0200

    Proyecto inicio

---
+++ DownMove/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace DownMove
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtRuta = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnOpenDialogo = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.lbRutaArchivo = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.lblLoading = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(489, 319);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Descargar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Url";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(41, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(497, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.Location = new System.Drawing.Point(391, 319);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Cerrar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(552, 32);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Descargas de MP4";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(55, 119);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(483, 10);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label3.Location = new System.Drawing.Point(18, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Origen";
+            // 
+            // txtRuta
+            // 
+            this.txtRuta.Location = new System.Drawing.Point(55, 67);
+            this.txtRuta.Name = "txtRuta";
+            this.txtRuta.Size = new System.Drawing.Size(453, 20);
+            this.txtRuta.TabIndex = 7;
+            this.txtRuta.Text = "C:\\_00\\Curso Xamarin\\";
+            this.txtRuta.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(5, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Ruta";
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label5.Location = new System.Drawing.Point(18, 147);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 24);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Destino";
+            // 
+            // panel1
+            // 
+            this.panel1.AccessibleDescription = "";
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.lbRutaArchivo);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.txtFile);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.btnOpenDialogo);
+            this.panel1.Controls.Add(this.progressBar1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txtRuta);
+            this.panel1.Location = new System.Drawing.Point(12, 163);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(552, 150);
+            this.panel1.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(8, 14);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Tipo";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "mp4",
+            "mp3",
+            "txt"});
+            this.comboBox1.Location = new System.Drawing.Point(55, 11);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 13;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btnOpenDialogo
+            // 
+            this.btnOpenDialogo.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnOpenDialogo.Location = new System.Drawing.Point(507, 65);
+            this.btnOpenDialogo.Name = "btnOpenDialogo";
+            this.btnOpenDialogo.Size = new System.Drawing.Size(31, 23);
+            this.btnOpenDialogo.TabIndex = 12;
+            this.btnOpenDialogo.Text = "...";
+            this.btnOpenDialogo.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnOpenDialogo.UseVisualStyleBackColor = true;
+            this.btnOpenDialogo.Click += new System.EventHandler(this.btnOpenDialogo_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(12, 54);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(552, 81);
+            this.panel2.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(5, 44);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Nombre";
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(55, 41);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(436, 20);
+            this.txtFile.TabIndex = 15;
+            this.txtFile.Text = "Xamarin";
+            this.txtFile.TextChanged += new System.EventHandler(this.txtFile_TextChanged);
+            // 
+            // lbRutaArchivo
+            // 
+            this.lbRutaArchivo.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbRutaArchivo.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.lbRutaArchivo.Location = new System.Drawing.Point(55, 94);
+            this.lbRutaArchivo.Name = "lbRutaArchivo";
+            this.lbRutaArchivo.Size = new System.Drawing.Size(483, 22);
+            this.lbRutaArchivo.TabIndex = 17;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(5, 94);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(43, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Destino";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(497, 44);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(48, 17);
+            this.checkBox1.TabIndex = 19;
+            this.checkBox1.Text = "Auto";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // lblLoading
+            // 
+            this.lblLoading.AutoSize = true;
+            this.lblLoading.BackColor = System.Drawing.Color.Transparent;
+            this.lblLoading.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lblLoading.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoading.ForeColor = System.Drawing.Color.Firebrick;
+            this.lblLoading.Location = new System.Drawing.Point(13, 321);
+            this.lblLoading.Name = "lblLoading";
+            this.lblLoading.Size = new System.Drawing.Size(0, 25);
+            this.lblLoading.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 354);
+            this.Controls.Add(this.lblLoading);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "DownMove";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtRuta;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnOpenDialogo;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lbRutaArchivo;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label lblLoading;
+    }
+}
+
---
+++ DownMove/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DownMove
+{
+    public partial class Form1 : Form
+    {
+        private string urlDefaul = ""; //"http://www.sample-videos.com/video/mp4/360/big_buck_bunny_360p_1mb.mp4";
+        private string rutaHD = "C:\\00\\video-" + DateTime.Now.Ticks + ".mp4";
+        private string nameFile = DateTime.Now.Ticks + ".mp4";
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = urlDefaul;
+            comboBox1.SelectedItem = "mp4";
+             nameFile += "mp4";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //var client = new WebClient();
+                //var uri = textBox1.Text;
+                //Uri address = new Uri(uri);
+                //client.DownloadFileAsync(address, rutaHD );
+                loading(true);
+                BtnDownload_Click();
+            }
+            catch(Exception ex) {
+                loading(false);
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+
+        private void BtnDownload_Click()
+        {
+            progressBar1.Value = 0;
+            rutaHD = getRuta() ;// "C:\\00\\video-" + DateTime.Now.Ticks + ".mp4";
+            using (WebClient wc = new WebClient())
+            {
+                wc.DownloadProgressChanged += wc_DownloadProgressChanged;
+                wc.DownloadFileAsync(new System.Uri(textBox1.Text),
+                rutaHD);
+            }
+        }
+
+        void wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage <= 10 ? 10 : e.ProgressPercentage;
+
+            if(progressBar1.Value >= 100)
+            {
+                loading(false);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        
+
+        private void btnOpenDialogo_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtRuta.Text = folderBrowserDialog1.SelectedPath;
+            }
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            setFile();
+        }
+
+        private void setFile()
+        {
+            var fileNameFull = txtFile.Text;
+            var subName = checkBox1.Checked ? DateTime.Now.Ticks.ToString() :"";
+            var tipo = comboBox1.Text;
+            var ruta = txtRuta.Text;
+             //ruta = ruta.LastIndexOf(@"\") == ruta.Length -1 ? ruta : ruta + @"\";
+            if (checkBox1.Checked)
+            {
+                fileNameFull = $"{fileNameFull}-{subName}.{tipo}";
+            }
+            else
+            {
+                fileNameFull = $"{fileNameFull}.{tipo}";
+            }
+            var rutaMasFile = System.IO.Path.Combine(ruta, fileNameFull);
+            lbRutaArchivo.Text =  string.Format("{0}{1}", ruta, fileNameFull);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            setFile();
+
+        }
+
+        private void txtFile_TextChanged(object sender, EventArgs e)
+        {
+            setFile();
+        }
+        private string getRuta()
+        {
+            return lbRutaArchivo.Text;
+        }
+
+        public void loading(bool isShow)
+        {
+            var value = isShow ? "Descargando, espere..." : "";
+            lblLoading.Text = value;
+
+        }
+    }
+}
